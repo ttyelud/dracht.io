ece44cafb6c320bbe9050f67bedc30d36d20f351
Dave Horton <daveh@beachdognet.com>
2018-04-12 13:00:23 -0400

    minor linting changes

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 //require('dotenv').load();
 
-const express = require('express')
-const pug = require('pug')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const ghost = require('ghost')
-const path = require('path')
-const tree = require('./tree')
-const fs = require('fs')
-const utils = require('./node_modules/ghost/core/server/services/url/utils')
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const ghost = require('ghost');
+const tree = require('./tree');
+const fs = require('fs');
 
-const app = express()
-const http = require('http').Server(app)
+const app = express();
+const http = require('http').Server(app);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const routes = require('./router')
+const routes = require('./router');
 
-app.set('port', (process.env.PORT || 8080))
-app.set('view engine', 'pug')
-app.use('/', routes)
-app.use(morgan('tiny'))
-app.use(express.static('public'))
+app.set('port', (process.env.PORT || 8080));
+app.set('view engine', 'pug');
+app.use('/', routes);
+app.use(morgan('tiny'));
+app.use(express.static('public'));
 
-app.locals.tutorials = tree('./docs/tutorials').children.slice(0,3);
+app.locals.tutorials = tree('./docs/tutorials').children.slice(0, 3);
 
-let conf = JSON.parse(fs.readFileSync('docs.conf.json'));
-console.log(conf['getting-started'])
+const conf = JSON.parse(fs.readFileSync('docs.conf.json'));
+console.log(conf['getting-started']);
 
-http.listen(app.get('port'), function() {
-  console.log('listening on *:' + app.get('port'))
-})
-
-ghost().then(function (ghostServer) {
-    app.use('/blog', ghostServer.rootApp);
-    ghostServer.start(app);
+http.listen(app.get('port'), () => {
+  console.log('listening on *:' + app.get('port'));
 });
+
+ghost().then((ghostServer) => {
+  app.use('/blog', ghostServer.rootApp);
+  return ghostServer.start(app);
+})
+  .catch((err) => {
+    console.error(`Error starting ghost: ${err}`);
+  });
