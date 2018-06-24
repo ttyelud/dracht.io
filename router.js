6d256a42fd8ba31f2de809712e34edcb0ca6c5b6
Dave Horton <daveh@beachdognet.com>
2018-06-24 21:25:07 +0100

    minor tweaks

--- router.js
+++ router.js
@@ -10,41 +10,37 @@ const docsTree = mdTree('./docs', './views');
 // Home
 
 router.get('/', function(req, res) {
-  const t = 'drachtio: the Node.js SIP application server';
+  const t = 'drachtio';
   const d = 'The node.js SIP application server framework.';
-  res.render('index', {title : t, description : d});
+  const k = 'drachtio, SIP, sip, sofia, drachtio-srf';
+  res.render('index', {title : t, description : d, keywords: k});
 });
-
-router.get('/api-test', function(req, res) {
-  const t = 'drachtio: the Node.js SIP application server';
-  const d = 'The node.js SIP application server framework.';
-  res.render('api', {title : t, description : d});
-});
-
 
 // Interior Pages
 
 router.get('/about', function(req, res) {
-  const t = 'About - drachtio';
+  const t = 'drachtio - about';
   const d = 'Learn more about drachtio, the node.js SIP application server framework.';
   res.render('about', {title : t, description : d});
 });
 
+/*
 router.get('/features', function(req, res) {
   const t = 'Features - drachtio';
   const d = 'Learn more about what drachtio has to offer.';
   res.render('features', {title : t, description : d});
 });
+*/
 
 router.get('/middleware', function(req, res) {
-  const t = 'drachtio middleware';
+  const t = 'drachtio - middleware';
   const d = 'drachtio middleware';
   res.render('middleware', {title : t, description : d});
 });
 
 router.get('/apps', function(req, res) {
-  const t = 'drachtio reference applications';
-  const d = 'Reference applications';
+  const t = 'drachtio - apps';
+  const d = 'drachtio reference apps';
   res.render('apps', {title : t, description : d});
 });
 
@@ -90,7 +86,7 @@ router.post('/contact', function(req, res) {
 
 router.get('/docs', (req, res) => {
   res.render('docs', {
-    title : 'drachtio developer docs',
+    title : 'drachtio - docs',
     description : 'Documentation for dracht.io, the node.js SIP application server framework.',
     tree: docsTree.children,
     active: 'developer-guide'
@@ -99,7 +95,7 @@ router.get('/docs', (req, res) => {
 
 
 router.get('/docs/:folder', (req, res) => {
-  const t = 'drachtio developer docs';
+  const t = 'drachtio - docs';
   const d = 'Documentation for drachtio, the node.js SIP application server framework.';
 
   const tree = _.find(docsTree.children, (c) => c.file === req.params.folder);
